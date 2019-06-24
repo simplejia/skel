e4b9799ff11fb17b5f7b8e27bb4be44cebd46ae8
simplejia <353474225@qq.com>
2019-06-24 15:01:54 +0800

    update new.sh

--- new.sh
+++ new.sh
@@ -35,7 +35,8 @@ echo "begin generate new project..."
 sed "s/package $pkg/package main/g" <main.go >main.go.new && mv main.go.new main.go && \
 sed "s/package $pkg/package main/g" <WSP.go >WSP.go.new && mv WSP.go.new WSP.go && \
 rm -rf tools skel new.sh install_tools.sh .git .gitignore ../${pkg}_api/.git && \
-gomvpkg -from $pkgpath/skel_api -to ${proj}_api)
+gomvpkg -from $pkgpath/skel_api -to ${proj}_api && \
+rm -rf ../${pkg}_api/.git)
 
 echo "begin download dependence..."
 go get -v github.com/simplejia/skel
